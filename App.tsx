baf7f80ca90a22d55c99e97362473b496d3f21cb
Elchen Tanauan <lchentanauan@gmail.com>
2023-07-24 15:40:39 +0800

    ScreenOrientaion Portrait and Landscape Working

--- App.tsx
+++ App.tsx
@@ -2,11 +2,22 @@ import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import StackNavigators from './android/app/src/ryori-android/src/Admin/navigation/stackNavigators';
 import {StatusBar} from 'react-native';
-// import UserNavigators from './android/app/src/ryori-android/src/user/navigation/userNavigators';
-
+import UserNavigators from './android/app/src/ryori-android/src/user/navigation/userNavigators';
+import {
+  OrientationLocker,
+  PORTRAIT,
+  LANDSCAPE,
+} from 'react-native-orientation-locker';
 const App = () => {
   return (
     <>
+      <OrientationLocker
+        orientation={LANDSCAPE}
+        onChange={orientation => console.log('onChange', orientation)}
+        onDeviceChange={orientation =>
+          console.log('onDeviceChange', orientation)
+        }
+      />
       {/* to hide status bar <StatusBar hidden/>  */}
       <StatusBar hidden />
       <NavigationContainer>
